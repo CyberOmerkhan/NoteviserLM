151c7224ac41de9d8cb15d022c36888e7d1d2494
Amirkhan <amazingOmerkhan@gmail.com>
2025-10-29 15:05:20 -0400

    v0.0.4

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ import { openai, supabase } from './config.js';
 import podcasts from './content.js';
 
 async function main(input) {
-  await Promise.all(
+  const data = await Promise.all(
     input.map( async (textChunk) => {
         const embeddingResponse = await openai.embeddings.create({
-            model: "text-embedding-ada-001",
+            model: "text-embedding-ada-002",
             input: textChunk
         });
-        const data = { 
+        return { 
           content: textChunk, 
           embedding: embeddingResponse.data[0].embedding 
         }
-        console.log(data);  
-    })    
+    })
   );
-  console.log('Embedding complete!');
+  
+  // Insert content and embedding into Supabase
+  await supabase.from('documents').insert(data); 
+  console.log('Embedding and storing complete!');
 }
+
+main(podcasts)
