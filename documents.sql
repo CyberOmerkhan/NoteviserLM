5ad0f998c7b6aa6b3e60665c25b13c388954a199
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-10-30 18:58:36 -0400

    v0.0.4

--- documents.sql
+++ documents.sql
@@ -4,11 +4,3 @@ create table documents (
   content text,
   embedding vector(1536)
 );
-=======
--- Create a table to store your documents
-create table documents (
-  id bigserial primary key,
-  content text, -- corresponds to the "text chunk"
-  embedding vector(1536) -- 1536 works for OpenAI embeddings
-);
->>>>>>> local-backup
