2e125e9ca65f35410a581c92763ecb93ae875dae
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-10-31 13:27:04 -0400

    v0.0.5

--- langchain_chunking.js
+++ langchain_chunking.js
@@ -1,10 +1,19 @@
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
 import * as fs from "node:fs";
 
 async function splitDocument(){
     const rawData = await fs.readFileSync("./podcasts.txt");
     const text = await rawData.toString();
-    console.log(text)
+
+    const textSplitter = new CharacterTextSplitter({
+        separator: " ",
+        chunkSize: 1000,
+        chunkOverlap: 200,
+    });
+
+    const texts = await textSplitter.createDocuments([text]);
+
+    console.log(texts);
 }
 
 splitDocument();
